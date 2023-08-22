ab2f00bcf5c5d72ff77cff4b42a7de3b9882f32f
Craig Drayton <craig@mazzlo.co>
2023-08-22 18:28:15 +1000

    Adds Koa server

--- src/feathers/app.mjs
+++ src/feathers/app.mjs
@@ -1,3 +1,14 @@
+import { feathers } from "@feathersjs/feathers";
+import {
+  koa,
+  rest,
+  bodyParser,
+  errorHandler,
+  serveStatic,
+} from "@feathersjs/koa";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+
 export default class MessageService {
   constructor() {
     this.messages = [];
@@ -24,4 +35,33 @@ export default class MessageService {
     this.messages.push(message);
     return message;
   }
+}
+
+// Creates an KoaJS compatible Feathers application
+export const app = koa(feathers());
+
+// Use the current folder for static file hosting
+app.use(serveStatic("."));
+
+// Register the error handle
+app.use(errorHandler());
+
+// Parse JSON request bodies
+app.use(bodyParser());
+
+// Register REST service handler
+app.configure(rest());
+
+// Register our messages service
+app.use("messages", new MessageService());
+
+export const startServer = () => {
+  app.listen(3030);
+  console.log("Feathers server listening on localhost:3030");
+};
+
+// If run as main, start the server
+/* istanbul ignore if */
+if (__filename === process.argv[1]) {
+  startServer();
 }
--- src/feathers/app.test.js
+++ src/feathers/app.test.js
@@ -1,5 +1,7 @@
 import { describe, expect, it } from "@jest/globals";
+import request from "supertest";
 import MessageService from "./app";
+import { app, startServer } from "./app";
 
 describe("MessageService", () => {
   it("Returns a list of messages", async () => {
@@ -26,5 +28,32 @@ describe("MessageService", () => {
   it("Throws if data.text is not a string", () => {
     const messages = new MessageService();
     expect(() => messages.create({ text: 1 })).toThrowError(TypeError);
-  })
+  });
+});
+
+describe("Koa server", () => {
+  it("Starts the server", async () => {
+    startServer();
+    const response = await request("http://localhost:3030").get("/messages");
+    expect(response.body).toEqual([]);
+    app.teardown();
+  });
+});
+
+describe("/messages", () => {
+  beforeAll(() => {
+    app.listen(3030);
+  });
+
+  afterAll(async () => {
+    app.teardown();
+  });
+
+  it("Handles POST and GET requests", async () => {
+    const input = { text: "testing /messages" };
+    await request("http://localhost:3030").post("/messages").send(input);
+
+    const response = await request("http://localhost:3030").get("/messages");
+    expect(response.body).toEqual([{ id: 0, ...input }]);
+  });
 });
